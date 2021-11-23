b264c4820decba79602a475384ef7224101dfbfe
Israan Gross <israanmendes@hotmail.com>
2021-11-22 19:04:42 -0800

    Assessment exam updated- Israan M

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,99 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
-
-// We'll use these variables to track the counts of each cookie type
-let gb = 0      // Gingerbread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
-
+let yourName = "Israan Mendes" 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
+// Variables tracking the counts of each cookie type
+// Gingerbread
+let gbVotesTd = document.querySelector("#qty-gb");  
+let gbVotes = localStorage.getItem('gbVotes');
+if (gbVotes === null) {
+    gbVotes = 0;
+} else{
+    gbVotes = parseInt(gbVotes)
+}
+gbVotesTd.textContent = gbVotes;
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+// Chocolate Chip
+let ccVotesTd = document.querySelector("#qty-cc");  
+let ccVotes = localStorage.getItem('ccVotes');
+if (ccVotes === null) {
+    ccVotes = 0;
+} else{
+    ccVotes = parseInt(ccVotes)
+}
+ccVotesTd.textContent = ccVotes;     
+
+// Sugar Sprinkle
+let ssVotesTd = document.querySelector("#qty-ss");  
+let ssVotes = localStorage.getItem('ssVotes');
+if (ssVotes === null) {
+    ssVotes = 0;
+} else{
+    ssVotes = parseInt(ssVotes)
+}
+ssVotesTd.textContent = ssVotes; 
+
+// Totals tracker
+let totalVotesTd = document.querySelector('#qty-total');
+totalVotesTd.textContent = gbVotes + ccVotes + ssVotes;
+
+// Selectors for each individual Pastry Cookie by div (altered in index.html ***added id***)
+let GBDiv = document.querySelector("#GB"); //Ginger bread (gb)
+let CCDiv = document.querySelector("#CC"); //Chocolate chip (cc)
+let SSDiv = document.querySelector("#SS"); //Sugar sprinkle (ss)
+
+
+// Event Listener for "+" button for Gingerbread cookie (gb)
+document.getElementById('add-gb').addEventListener('click', function() {
+    gbVotes = gbVotes + 1;
+    gbVotesTd.textContent = gbVotes;
+    localStorage.setItem('gbVotes',gbVotes);
+
+    totalVotesTd.textContent = gbVotes + ccVotes + ssVotes;
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+// Event Listener for "-" button for Gingerbread cookie (gb)
+document.getElementById('minus-gb').addEventListener('click',function(e) {
+    gbVotes = gbVotes - 1;
+    gbVotesTd.textContent = gbVotes;
+    localStorage.setItem('gbVotes',gbVotes);
+
+    totalVotesTd.textContent = gbVotes + ccVotes + ssVotes;
+})
+
+// Event Listener for "+" button for Chocolate chip cookie (cc)
+document.getElementById('add-cc').addEventListener('click', function() {
+    ccVotes = ccVotes + 1;
+    ccVotesTd.textContent = ccVotes;
+    localStorage.setItem('ccVotes',ccVotes);
+
+    totalVotesTd.textContent = gbVotes + ccVotes + ssVotes;
+})
+
+// Event Listener for "-" button for Chocolate chip cookie (cc)
+document.getElementById('minus-cc').addEventListener('click',function(e) {
+    ccVotes = ccVotes - 1;
+    ccVotesTd.textContent = ccVotes;
+    localStorage.setItem('ccVotes',ccVotes);
+
+    totalVotesTd.textContent = gbVotes + ccVotes + ssVotes;
+})
+
+// Event Listener for "+" button for Sugar sprinkle cookie (ss)
+document.getElementById('add-ss').addEventListener('click', function() {
+    ssVotes = ssVotes + 1;
+    ssVotesTd.textContent = ssVotes;
+    localStorage.setItem('ssVotes',ssVotes);
+
+    totalVotesTd.textContent = gbVotes + ccVotes + ssVotes;
+})
+
+// Event Listener for "-" button for Sugar sprinkle cookie (ss)
+document.getElementById('minus-ss').addEventListener('click',function(e) {
+    ssVotes = ssVotes - 1;
+    ssVotesTd.textContent = ssVotes;
+    localStorage.setItem('ssVotes',ssVotes);
+
+    totalVotesTd.textContent = gbVotes + ccVotes + ssVotes;
+})
